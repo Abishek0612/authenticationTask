73fc3c8071f41092829d5b89b2f395395e94105c
Abishek0612 <uabishek6@gmail.com>
2023-04-18 19:47:04 +0530

    dotenv

--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const dbConnect = async() => {
     try{
-        const connected = await mongoose.connect("mongodb://0.0.0.0:27017/authentication")
+        const connected = await mongoose.connect(process.env.MONGO_URL)
         console.log(`Mongodb connected `)
     }catch(error){
         console.log(`Error : ${error.message}`)
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dbConnect  from "./config/dbConnect.js";
 import userRoutes from "./routes/usersRoutes.js";
+import dotenv from "dotenv";
+dotenv.config();
 import cors from 'cors'
 //db connection
 dbConnect()
